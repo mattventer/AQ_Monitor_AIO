35ef7d45d9740cd7537a7de2915a02345c3db238
mattventer <venterke@gmail.com>
2019-11-19 10:04:46 -0500

    removed  comments in start

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 rm src/aq_monitor.log src/aq_sms_responder.log
 NOW=$(date +"%m-%d-%Y_%r")
-echo "New session: $NOW" >> src/aq_monitor.log >> src/aq_sms_responder.log
+echo "New session: $NOW" >> src/aq_monitor.log 
+echo "New session: $NOW" >> src/aq_sms_responder.log
 echo "Starting AQ AIO..."
 echo "Starting AQ AIO..." >> src/aq_monitor.log
 nohup python3 src/aq_monitor.py >> src/aq_monitor.log &
-#echo "Starting ngrok server with subdomain aqmonitor"
-#echo "Starting ngrok server with subdomain aqmonitor" >> src/aq_sms_responder.log
-#cd src/
-#./ngrok http -subdomain=aq_monitor 5000 >> src/aq_sms_responder.log &
-#cd ..
 echo "Starting AQ SMS Responder..."
 echo "Starting AQ SMS Responder..." >> src/aq_sms_responder.log
 nohup python3 src/aq_sms_responder.py >> src/aq_sms_responder.log &
